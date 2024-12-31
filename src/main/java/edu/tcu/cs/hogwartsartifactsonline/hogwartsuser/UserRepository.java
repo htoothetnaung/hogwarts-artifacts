6ccaab7d15ef068754a624a90a4b6c2bd9e92e20
Htoo Thet <92380477+htoothetnaung@users.noreply.github.com>
2024-12-31 12:16:41 +0630

    User Authentication and Authorization Done! Close #5

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserRepository.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserRepository.java
@@ -2,5 +2,11 @@ package edu.tcu.cs.hogwartsartifactsonline.hogwartsuser;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<HogwartsUser, Integer> {
+
+    Optional<HogwartsUser> findByUsername(String username); // we use optional to consider not found case
+
+    Optional <HogwartsUser> findByUsernameAndPassword(String username, String password);
 }
